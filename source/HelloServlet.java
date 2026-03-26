e2d31b6a65dc4d65adf6b7777954d2b937128ac1
Yevhenii Dmytriiev <yevhenii.dmytriiev@rci.rogers.com>
2026-03-26 11:55:06 -0400

    poc app added

---
+++ source/HelloServlet.java
@@ -0,0 +1,20 @@
+package org.example;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(urlPatterns = {"/hello"})
+public class HelloServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+
+        resp.setContentType("text/plain; charset=UTF-8");
+        resp.getWriter().write("Hello from the custom application running on top of k8s (Weblogic + OSB domain) !!!");
+    }
+}
